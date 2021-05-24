f50f240148df742e454eff9eabeb40076439e158
Aishwarya <aishwaryabr6@gmail.com>
2021-05-24 16:07:32 +0530

    added react assignment

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import MyCard from './MyCard';
+//import Cardnews from './Cardnews';
+//import './App.css';
+import Vote from './Components/Vote';
+
+ class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+            <Vote/>
+         
+            </div>
+        )
+    }
+}
+
+
+
+// export default App;
+
+//import logo from './logo.svg';
+//import './App.css';
+
+//import IncreDecre from './Components/IncreDecre'
+
+
+// function App() {
+
+//   return (
+//      //<IncreDecre/>
+//   //<MyCard/>
+//   <Cardnews/>
+//   );
+// }
+
+export default App;
